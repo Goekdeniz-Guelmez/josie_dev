d62d47073eaf12d67c0addf3fcb8280a2a49d1dc
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-10-27 14:21:16 +0100

    update

--- audio.py
+++ audio.py
@@ -4,8 +4,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .args import AudioRQTransformer as ModelArgs
+from .args import AudioRQTransformerArgs as ModelArgs
 
+
+# TODO add Spectral, Temporal, Depth codeblocks and 
 
 class DualAudioStreamRQTransformer(nn.Module):
     def __init__(self, args: ModelArgs):
--- utils.py
+++ utils.py
@@ -1,1 +1,4 @@
 # TODO add ROPE stuff
+# TODO add RMS Norm class
+# TODO add precompute_freqs_cis function
+# TODO add repeat_kv function
