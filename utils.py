bd25b9e7d5214e319c15621d2d61254e8b0a48e8
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-10-27 15:01:14 +0100

    adding RMSNorm to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
+import torch
+from torch import nn
+
 # TODO add ROPE stuff
-# TODO add RMS Norm class
 # TODO add precompute_freqs_cis function
 # TODO add repeat_kv function
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, hidden_size: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keephidden_size=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
