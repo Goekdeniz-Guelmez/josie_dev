c0eafc5e164b2e491e128236c20f27746acda1b5
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-12-06 14:37:35 +0100

    changing up some modelargs

--- v4/args.py
+++ v4/args.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, field
 from typing import Optional, Type
 from pathlib import Path
 
-import pyaudio
 import inspect
 
 
@@ -16,22 +15,6 @@ class BaseModelArgs:
             if k in inspect.signature(cls).parameters
         }
         return cls(**valid_params)
-
-
-@dataclass
-class InferenceArgs(BaseModelArgs):
-    format = pyaudio.paFloat32
-    channels = 1
-    rate = 16000  # 16kHz
-    record_seconds = 0.25  # 250ms
-    chunk = rate // 4 # 4096
-
-
-@dataclass
-class StreamingArgs(BaseModelArgs):
-    chunk_size: int = 512
-    context_size: int = 2048
-    overlap_size: int = 128
 
 
 @dataclass
@@ -93,27 +76,38 @@ class VisionEncoderModelArgs(BaseModelArgs):
 
 
 @dataclass
+class TemporialTransformer(BaseModelArgs):
+    hidden_size: int = 1028
+    hidden_layers: int = 12
+    num_heads: int = 12
+    head_dim: int = hidden_size // num_heads
+
+    rms_norm_eps: float = 1e-5
+    max_position_embeddings: int = 4096
+
+
+@dataclass
+class DepthTransformer(BaseModelArgs):
+    hidden_size: int = 512
+    hidden_layers: int = 6
+    num_heads: int = 4
+    head_dim: int = hidden_size // num_heads
+
+    rms_norm_eps: float = 1e-5
+    max_position_embeddings: int = 4096
+
+
+@dataclass
 class ModelArgs(BaseModelArgs):
     audio_encoder_args: Type[AudioEncoderModelArgs] = AudioEncoderModelArgs
     audio_decoder_args: Type[AudioDecoderModelArgs] = AudioDecoderModelArgs
     vision_encoder_args: Type[VisionEncoderModelArgs] = VisionEncoderModelArgs
 
-    inference_args: Type[InferenceArgs] = InferenceArgs
-    streaming_args: Type[StreamingArgs] = StreamingArgs
+    temorial_transformer: Type[TemporialTransformer] = TemporialTransformer
+    depth_transformer: Type[DepthTransformer] = DepthTransformer
 
-    architecture: str = 'LlamaForCausalLM'
-    hidden_size: int = 896
-    hidden_layers: int = 24
-    num_heads: int = 14
-    num_kv_heads: Optional[int] = 2
-    head_dim: int = hidden_size // num_heads
-    rms_norm_eps: float = 1e-06
-    attention_dropout: float = 0.0
-    max_position_embeddings: int = 1028
-    rope_theta: float = 1000000.0
+    stfu_token_id: int = 0
+
     vocab_size: int = 128256
-    multiple_of: int = 256
-    ffn_dim_multiplier: Optional[float] = None
 
     tokenizer_path: Path = field(default=Path('/Users/gokdenizgulmez/Desktop/J.O.S.I.E./tokenizer.model'))
-    batch_size: int = 2
